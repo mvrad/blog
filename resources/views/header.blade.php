5f5c0c8d18d659bd7d2c7e747fcfe7f62284ad16
Matthew Conrad <matthew.conrad@outlook.com>
2019-07-14 04:19:43 -0700

    Adjust media queries for header.blade.php elements

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,9 @@
 <header class="blog-header py-3 mb-3">
-  <div class="row flex-nowrap justify-content-between align-items-center">
-    <div class="col-5">
+  <div class="row flex-nowrap justify-content-between align-items-center header-flex">
+    <div class="name">
       <a class="blog-header-logo text-dark" href="#">Matt Conrad | Blog</a>
     </div>
-    <div class="col-4 d-flex justify-content-end align-items-center">
+    <div class="col-4 d-flex align-items-center">
       <a class="pr-3" href="/">Home</a>
       <a href="https://mconrad.me" target="_blank">Portfolio</a>
     </div>
