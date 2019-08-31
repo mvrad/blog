c84a0f430374253858c025a0769ca97b5cc9ded4
Matthew Conrad <matthew.conrad@outlook.com>
2019-08-30 23:58:09 -0700

    Remove unused Blade templates

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -19,24 +19,6 @@
         <p class="blog-post-meta">
           <!-- Publish date -->
           {{$published}}
-          
-          by
-
-          <!-- Post author -->
-          <a href="/author/{{urlencode($post->getAuthor()->getSlug())}}">
-            {{$post->getAuthor()->getFirstName()}} {{$post->getAuthor()->getLastName()}}
-          </a>
-
-          <br />
-
-          @if (count($post->getCategories()) > 0)  
-            categories: 
-          @endif
-
-          <!-- Post categories -->
-          @foreach ($post->getCategories() as $category)
-            <a href="/category/{{urlencode($category->getSlug())}}">{{$category->getName()}}</a>
-          @endforeach
         </p>
         <p>
           {!! $post->getBody() !!}
